aa720a532b63bfa3f7905c7c34dca7261a321edb
Aaron Su <xunzesu@outlook.com>
2019-08-26 22:01:36 -0400

    Add section 9 desgin patterns with a map with marker example

---
+++ section-09-design-patterns/maps/src/index.ts
@@ -0,0 +1,12 @@
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+
+// console.log(user);
+
+const map = new CustomMap("map");
+map.addMarker(user);
+map.addMarker(company);
